c85857da3dd702427458357955e0bacb148a0b9a
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-10-11 13:15:24 +0600

    readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -10,6 +14,9 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'no-unused-vars': 'off',
