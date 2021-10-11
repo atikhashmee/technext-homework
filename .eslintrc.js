8040dbbd82fc3c3fb5e0a89b0148cc8ede80b49e
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-10-11 12:56:23 +0600

    deploy issue fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,6 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'no-unused-vars': 'off',
